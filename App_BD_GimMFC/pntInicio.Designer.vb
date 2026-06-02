c84b1cfdb0047bd777bbaa69d7ac2c5135272311
myrarimoreno-design <myrarimoreno@gmail.com>
2026-06-02 13:31:08 -0600

    Update pntInicio.Designer.vb
    mover bienvenido

--- App_BD_GimMFC/pntInicio.Designer.vb
+++ App_BD_GimMFC/pntInicio.Designer.vb
@@ -41,10 +41,9 @@ Partial Class pntInicio
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Times New Roman", 21.85714!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(466, 68)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label1.Location = New System.Drawing.Point(57, 40)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(269, 58)
+        Me.Label1.Size = New System.Drawing.Size(156, 35)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Bienvenido"
         '
@@ -52,10 +51,9 @@ Partial Class pntInicio
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(364, 510)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label2.Location = New System.Drawing.Point(199, 276)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(153, 29)
+        Me.Label2.Size = New System.Drawing.Size(90, 16)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Contraseña:"
         '
@@ -63,45 +61,40 @@ Partial Class pntInicio
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.857143!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(364, 351)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label3.Location = New System.Drawing.Point(199, 190)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(110, 29)
+        Me.Label3.Size = New System.Drawing.Size(69, 17)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Usuario:"
         '
         'txtUsuario
         '
-        Me.txtUsuario.Location = New System.Drawing.Point(538, 351)
-        Me.txtUsuario.Margin = New System.Windows.Forms.Padding(6)
+        Me.txtUsuario.Location = New System.Drawing.Point(293, 190)
         Me.txtUsuario.Name = "txtUsuario"
-        Me.txtUsuario.Size = New System.Drawing.Size(255, 29)
+        Me.txtUsuario.Size = New System.Drawing.Size(141, 20)
         Me.txtUsuario.TabIndex = 3
         '
         'txtContraseña
         '
-        Me.txtContraseña.Location = New System.Drawing.Point(538, 511)
-        Me.txtContraseña.Margin = New System.Windows.Forms.Padding(6)
+        Me.txtContraseña.Location = New System.Drawing.Point(293, 277)
         Me.txtContraseña.Name = "txtContraseña"
-        Me.txtContraseña.Size = New System.Drawing.Size(255, 29)
+        Me.txtContraseña.Size = New System.Drawing.Size(141, 20)
         Me.txtContraseña.TabIndex = 4
         '
         'btnIniciar
         '
-        Me.btnIniciar.Location = New System.Drawing.Point(804, 653)
-        Me.btnIniciar.Margin = New System.Windows.Forms.Padding(6)
+        Me.btnIniciar.Location = New System.Drawing.Point(439, 354)
         Me.btnIniciar.Name = "btnIniciar"
-        Me.btnIniciar.Size = New System.Drawing.Size(138, 42)
+        Me.btnIniciar.Size = New System.Drawing.Size(75, 23)
         Me.btnIniciar.TabIndex = 5
         Me.btnIniciar.Text = "Acceder"
         Me.btnIniciar.UseVisualStyleBackColor = True
         '
         'btnSalir
         '
-        Me.btnSalir.Location = New System.Drawing.Point(1012, 653)
-        Me.btnSalir.Margin = New System.Windows.Forms.Padding(6)
+        Me.btnSalir.Location = New System.Drawing.Point(552, 354)
         Me.btnSalir.Name = "btnSalir"
-        Me.btnSalir.Size = New System.Drawing.Size(138, 42)
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
         Me.btnSalir.TabIndex = 6
         Me.btnSalir.Text = "Salir"
         Me.btnSalir.UseVisualStyleBackColor = True
@@ -109,9 +102,10 @@ Partial Class pntInicio
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(228, 328)
+        Me.PictureBox1.Location = New System.Drawing.Point(124, 178)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(100, 68)
+        Me.PictureBox1.Size = New System.Drawing.Size(55, 37)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 7
         Me.PictureBox1.TabStop = False
@@ -119,9 +113,10 @@ Partial Class pntInicio
         'PictureBox2
         '
         Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(228, 486)
+        Me.PictureBox2.Location = New System.Drawing.Point(124, 263)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(100, 68)
+        Me.PictureBox2.Size = New System.Drawing.Size(55, 37)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox2.TabIndex = 8
         Me.PictureBox2.TabStop = False
@@ -130,17 +125,18 @@ Partial Class pntInicio
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.857143!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(223, 229)
+        Me.Label4.Location = New System.Drawing.Point(122, 124)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(570, 29)
+        Me.Label4.Size = New System.Drawing.Size(361, 17)
         Me.Label4.TabIndex = 9
         Me.Label4.Text = "Para comenzar ingrese su usuario y contraseña."
         '
         'pntInicio
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1274, 773)
+        Me.ClientSize = New System.Drawing.Size(695, 406)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.PictureBox1)
@@ -151,7 +147,6 @@ Partial Class pntInicio
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Margin = New System.Windows.Forms.Padding(6)
         Me.Name = "pntInicio"
         Me.Text = " "
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
