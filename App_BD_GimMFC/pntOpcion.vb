520db6c3d256d32952d5fe305a20a0c52c0a1474
myrarimoreno-design <myrarimoreno@gmail.com>
2026-06-02 16:59:31 -0600

    corrección de los botones pntOpcion

--- App_BD_GimMFC/pntOpcion.vb
+++ App_BD_GimMFC/pntOpcion.vb
@@ -25,23 +25,28 @@
     End Sub
 
     Private Sub btnEstudiante_Click(sender As Object, e As EventArgs) Handles btnEstudiante.Click
-        'crear una nueva instancia del formulario pntEstudiante'
+        Me.Hide() ' ocultar pntOpcion
         Dim frmEstudiante As New pntEstudiante()
-
-        'mostrar el formulario en pantalla'
-        frmEstudiante.ShowDialog()
+        frmEstudiante.ShowDialog() ' mostrar como modal
+        Me.Show() ' volver a mostrar pntOpcion al cerrar
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClases.Click
-        pntClases.ShowDialog()
+    Private Sub btnClases_Click(sender As Object, e As EventArgs) Handles btnClases.Click
+        Me.Hide()
+        Dim frmClases As New pntClases()
+        frmClases.ShowDialog()
+        Me.Show()
     End Sub
 
     Private Sub PictureBox3_Click(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles bntEmpleados.Click
-        pntEmpleados.ShowDialog()
+    Private Sub bntEmpleados_Click(sender As Object, e As EventArgs) Handles bntEmpleados.Click
+        Me.Hide()
+        Dim frmEmpleados As New pntEmpleados()
+        frmEmpleados.ShowDialog()
+        Me.Show()
     End Sub
 
     Private Sub btnRegresarEst_Click(sender As Object, e As EventArgs) Handles btnRegresarEst.Click
@@ -49,10 +54,16 @@
     End Sub
 
     Private Sub btnVentas_Click(sender As Object, e As EventArgs) Handles btnVentas.Click
-        pntVentas.ShowDialog()
+        Me.Hide()
+        Dim frmVentas As New pntVentas()
+        frmVentas.ShowDialog()
+        Me.Show()
     End Sub
 
     Private Sub btnCompras_Click(sender As Object, e As EventArgs) Handles btnCompras.Click
-        pntCompras.ShowDialog()
+        Me.Hide()
+        Dim frmCompras As New pntCompras()
+        frmCompras.ShowDialog()
+        Me.Show()
     End Sub
 End Class
