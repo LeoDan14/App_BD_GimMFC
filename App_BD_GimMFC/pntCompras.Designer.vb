b5b04f69ece01ea4dd71595d284bd9aa92fac030
myrarimoreno-design <myrarimoreno@gmail.com>
2026-06-03 01:34:36 -0600

    avance pntCompras

--- App_BD_GimMFC/pntCompras.Designer.vb
+++ App_BD_GimMFC/pntCompras.Designer.vb
@@ -26,26 +26,32 @@ Partial Class pntCompras
         Me.Label4 = New System.Windows.Forms.Label()
         Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         Me.btnRegresarComp = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnBuscarCompras = New System.Windows.Forms.Button()
+        Me.cbOpcionFecha = New System.Windows.Forms.ComboBox()
+        Me.dgvCompras = New System.Windows.Forms.DataGridView()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvCompras, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(261, 111)
-        Me.Label4.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(104, 41)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(183, 48)
+        Me.Label4.Size = New System.Drawing.Size(141, 36)
         Me.Label4.TabIndex = 4
         Me.Label4.Text = "Compras"
         '
         'PictureBox3
         '
         Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
-        Me.PictureBox3.Location = New System.Drawing.Point(119, 95)
+        Me.PictureBox3.Location = New System.Drawing.Point(29, 32)
+        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(109, 83)
+        Me.PictureBox3.Size = New System.Drawing.Size(59, 45)
         Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox3.TabIndex = 19
         Me.PictureBox3.TabStop = False
@@ -55,25 +61,70 @@ Partial Class pntCompras
         Me.btnRegresarComp.Font = New System.Drawing.Font("Mongolian Baiti", 8.142858!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnRegresarComp.Image = CType(resources.GetObject("btnRegresarComp.Image"), System.Drawing.Image)
         Me.btnRegresarComp.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.btnRegresarComp.Location = New System.Drawing.Point(924, 635)
+        Me.btnRegresarComp.Location = New System.Drawing.Point(507, 336)
+        Me.btnRegresarComp.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnRegresarComp.Name = "btnRegresarComp"
-        Me.btnRegresarComp.Size = New System.Drawing.Size(130, 56)
+        Me.btnRegresarComp.Size = New System.Drawing.Size(95, 46)
         Me.btnRegresarComp.TabIndex = 21
         Me.btnRegresarComp.Text = "Regresar"
         Me.btnRegresarComp.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.btnRegresarComp.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(107, 86)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(271, 15)
+        Me.Label1.TabIndex = 23
+        Me.Label1.Text = "Selecciona el mes, para ver el registro de ventas:"
+        '
+        'btnBuscarCompras
+        '
+        Me.btnBuscarCompras.Image = CType(resources.GetObject("btnBuscarCompras.Image"), System.Drawing.Image)
+        Me.btnBuscarCompras.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnBuscarCompras.Location = New System.Drawing.Point(413, 86)
+        Me.btnBuscarCompras.Name = "btnBuscarCompras"
+        Me.btnBuscarCompras.Size = New System.Drawing.Size(106, 46)
+        Me.btnBuscarCompras.TabIndex = 25
+        Me.btnBuscarCompras.Text = "Buscar"
+        Me.btnBuscarCompras.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnBuscarCompras.UseVisualStyleBackColor = True
+        '
+        'cbOpcionFecha
+        '
+        Me.cbOpcionFecha.FormattingEnabled = True
+        Me.cbOpcionFecha.Location = New System.Drawing.Point(110, 111)
+        Me.cbOpcionFecha.Name = "cbOpcionFecha"
+        Me.cbOpcionFecha.Size = New System.Drawing.Size(178, 21)
+        Me.cbOpcionFecha.TabIndex = 26
+        '
+        'dgvCompras
+        '
+        Me.dgvCompras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvCompras.Location = New System.Drawing.Point(46, 159)
+        Me.dgvCompras.Name = "dgvCompras"
+        Me.dgvCompras.Size = New System.Drawing.Size(600, 150)
+        Me.dgvCompras.TabIndex = 27
+        '
         'pntCompras
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1274, 773)
+        Me.ClientSize = New System.Drawing.Size(695, 406)
+        Me.Controls.Add(Me.dgvCompras)
+        Me.Controls.Add(Me.cbOpcionFecha)
+        Me.Controls.Add(Me.btnBuscarCompras)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnRegresarComp)
         Me.Controls.Add(Me.PictureBox3)
         Me.Controls.Add(Me.Label4)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "pntCompras"
         Me.Text = "pntCompras"
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvCompras, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -82,4 +133,8 @@ Partial Class pntCompras
     Friend WithEvents Label4 As Label
     Friend WithEvents PictureBox3 As PictureBox
     Friend WithEvents btnRegresarComp As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnBuscarCompras As Button
+    Friend WithEvents cbOpcionFecha As ComboBox
+    Friend WithEvents dgvCompras As DataGridView
 End Class
--- App_BD_GimMFC/pntCompras.vb
+++ App_BD_GimMFC/pntCompras.vb
@@ -1,9 +1,72 @@
-﻿Public Class pntCompras
-    Private Sub btnRegresarComp_Click(sender As Object, e As EventArgs) Handles btnRegresarComp.Click
+﻿Imports System.Data.SqlClient
 
+Public Class pntCompras
+
+    Private Sub btnRegresarComp_Click(sender As Object, e As EventArgs) Handles btnRegresarComp.Click
+        Me.Close()
     End Sub
 
     Private Sub pntCompras_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Close()
+        ' llenar combo con pares Año-Mes en formato numérico
+        cbOpcionFecha.Items.Add("2026-01")
+        cbOpcionFecha.Items.Add("2026-02")
+        cbOpcionFecha.Items.Add("2026-03")
     End Sub
+
+    Private Sub btnBuscarCompras_Click(sender As Object, e As EventArgs) Handles btnBuscarCompras.Click
+        Dim conexion As New SqlConnection("Data Source=DESKTOP-P1KRNOI\SQLEXPRESS;Initial Catalog=Gimnasio;Integrated Security=True")
+
+        If cbOpcionFecha.SelectedItem Is Nothing Then
+            MsgBox("Seleccione un año-mes en el combo antes de buscar.", MsgBoxStyle.Exclamation, "Aviso")
+            Exit Sub
+        End If
+
+        ' ejemplo: "2026-01"
+        Dim seleccion As String = cbOpcionFecha.SelectedItem.ToString()
+        Dim partes() As String = seleccion.Split("-"c)
+        Dim año As Integer = Convert.ToInt32(partes(0))
+        Dim mes As Integer = Convert.ToInt32(partes(1))
+
+        ' consulta directa con JOIN
+        Dim Query As String = "SELECT 
+                                   c.IdCompras,
+                                   c.Descripcion,
+                                   c.Fecha_Comp,
+                                   c.Monto_Comp,
+                                   e.Nombre_Empleado,
+                                   p.Nombre_Proveedor,
+                                   a.Nombre_Art,
+                                   dc.Cantidad
+                               FROM Compras c
+                               INNER JOIN Empleado e ON c.IdEmpleado = e.IdEmpleado
+                               INNER JOIN Proveedor p ON c.IdProv = p.IdProv
+                               INNER JOIN Detalle_Compra dc ON c.IdCompras = dc.IdCompras
+                               INNER JOIN Articulo a ON dc.IdArticulo = a.IdArticulo
+                               WHERE MONTH(c.Fecha_Comp) = @mes AND YEAR(c.Fecha_Comp) = @anio
+                               ORDER BY c.IdCompras, c.Fecha_Comp"
+
+        Dim cmd As New SqlClient.SqlCommand(Query, conexion)
+        cmd.Parameters.AddWithValue("@mes", mes)
+        cmd.Parameters.AddWithValue("@anio", año)
+
+        Try
+            conexion.Open()
+            Dim adapter As New SqlClient.SqlDataAdapter(cmd)
+            Dim tabla As New DataTable()
+            adapter.Fill(tabla)
+
+            dgvCompras.DataSource = tabla
+
+            If tabla.Rows.Count = 0 Then
+                MsgBox("No se encontraron compras para el año-mes seleccionado.", MsgBoxStyle.Exclamation, "Aviso")
+            End If
+
+        Catch ex As Exception
+            MsgBox("Error al buscar compras: " & ex.Message, MsgBoxStyle.Critical, "Error")
+        Finally
+            conexion.Close()
+        End Try
+    End Sub
+
 End Class
+
